59649607089a313162a83bb253f58eb7bda7a784
Larry Zhang <lzhang124@gmail.com>
2015-01-11 15:31:06 -0500

    fixed open sans

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/login', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
