73ae3c7ed3386a2bfb65c8253777709739d3173a
Rose Wang <rzwang@mit.edu>
2015-01-11 02:12:17 -0500

    linking to other pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res) {
   res.render('index', { title: '6148 Web Application' });
 });
 
+router.get('/login', function(req, res){
+    res.render('login', {title: 'Login'});
+});
+
+router.get('/signup', function(req, res){
+    res.render('signup', {title: 'Sign Up'});
+});
+
+router.get('/request_match', function(req, res){
+    res.render('requrest_match', {title: 'Request Match'});
+});
+
+router.get('/results', function(req, res){
+    res.render('results', {title: 'Results'});
+});
+
 module.exports = router;
