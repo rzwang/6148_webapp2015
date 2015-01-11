3ae3eebd84f4e8b1f49b8652d6558b4df488b981
Larry Zhang <lzhang124@gmail.com>
2015-01-11 02:23:44 -0500

    fixed app.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: '6148 Web Application' });
+  res.render('index', { title: '6.148 Web Application' });
 });
 
 module.exports = router;
