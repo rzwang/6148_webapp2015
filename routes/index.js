32a89b542e77bfb7d78c74cb672ec773b128bde5
Larry Zhang <lzhang124@gmail.com>
2015-01-11 02:38:41 -0500

    minor bug fixes

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: '6.148 Web Application' });
+  res.render('index', { title: 'ridesplitr' });
 });
 
-router.get('/login', function(req, res){
+router.get('/login', function(req, res) {
     res.render('login', {title: 'Login'});
 });
 
-router.get('/signup', function(req, res){
-    res.render('signup', {title: 'Sign Up'});
-});
+// // ADD THESE BACK IN WHEN THE VIEWS FILES ARE CREATED
 
-router.get('/request_match', function(req, res){
-    res.render('requrest_match', {title: 'Request Match'});
-});
+// router.get('/signup', function(req, res) {
+//     res.render('signup', {title: 'Sign Up'});
+// });
 
-router.get('/results', function(req, res){
-    res.render('results', {title: 'Results'});
-});
+// router.get('/request_match', function(req, res) {
+//     res.render('request_match', {title: 'Request Match'});
+// });
+
+// router.get('/results', function(req, res) {
+//     res.render('results', {title: 'Results'});
+// });
 
 module.exports = router;
