8d076ee1d67ab8d9187c1b7196b7e08050af1842
Larry Zhang <lzhang124@gmail.com>
2015-01-11 20:11:44 -0500

    hitch title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'ridesplitr' });
+  res.render('index', { title: 'hitch' });
 });
 
 router.get('/login', function(req, res) {
