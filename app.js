706ad711dd1623c7ddd4af5c913c327205169a3a
Larry Zhang <lzhang124@gmail.com>
2015-01-12 03:17:50 -0500

    button formatting

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/login', routes)
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
