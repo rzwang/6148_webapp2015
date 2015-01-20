8125bab397129dbc45b5ca953055cc5aabca72f2
Larry Zhang <lzhang124@gmail.com>
2015-01-20 18:06:58 -0500

    added port

--- app.js
+++ app.js
@@ -77,8 +77,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
-// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-// var server = app.listen(app.get('port'));
+var server = app.listen(app.get('port'));
 
 module.exports = app;
