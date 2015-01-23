5f1be1f3d22abed21ec089d7cff7e70f17d4d3d3
Larry Zhang <lzhang124@gmail.com>
2015-01-22 19:56:44 -0500

    npm update

---
+++ models/requestModel.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Request = new Schema({
+    id: String,
+    firstname: String,
+    lastname: String,
+    pickup: String,
+    pickup_loc: String,
+    dropoff: String,
+    dropoff_loc: String,
+    date: String,
+    time_disp: String,
+    time_calc: Number,
+    phone: String,
+    results: Array
+});
+
+module.exports = mongoose.model('Request', Request);
