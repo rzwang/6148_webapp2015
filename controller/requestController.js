283cf184d875a6575d86c288766edbd45af90d21
Larry Zhang <lzhang124@gmail.com>
2015-01-24 02:03:06 -0500

    schema fixed

--- controller/requestController.js
+++ controller/requestController.js
@@ -5,9 +5,15 @@ var createRequest = function(req, res){
         firstname: req.user.firstname,
         lastname: req.user.lastname,
         pickup: req.body['pickup'],
-        pickup_loc: req.body['pickup_loc'].split(', '),
+        pickup_loc: {
+            lat: req.body['pickup_loc'].split(', ')[0],
+            lng: req.body['pickup_loc'].split(', ')[1]
+        },
         dropoff: req.body['dropoff'],
-        dropoff_loc: req.body['dropoff_loc'].split(', '),
+        dropoff_loc: {
+            lat: req.body['dropoff_loc'].split(', ')[0],
+            lng: req.body['dropoff_loc'].split(', ')[1]
+        },
         date: req.body['date'],
         time_disp: req.body['time'],
         time_calc: req.body['time_calc'],
