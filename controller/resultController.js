bef8747961d592f04a403918600ab3721bc487e2
Larry Zhang <lzhang124@gmail.com>
2015-01-24 05:08:34 -0500

    fixed sort

--- controller/resultController.js
+++ controller/resultController.js
@@ -12,11 +12,11 @@ function compare(request) {
         var a_dropoff_lngDiff = Math.abs(request.dropoff_lng - a.dropoff_lng);
         var aDiff = a_timeDiff/hour + a_pickup_latDiff/lat2 + a_pickup_lngDiff/lng2 + a_dropoff_latDiff/lat2 + a_dropoff_lngDiff/lng2;
 
-        var b_timeDiff = Math.abs(request.time_calc - a.time_calc);
-        var b_pickup_latDiff = Math.abs(request.pickup_lat - a.pickup_lat);
-        var b_pickup_lngDiff = Math.abs(request.pickup_lng - a.pickup_lng);
-        var b_dropoff_latDiff = Math.abs(request.dropoff_lat - a.dropoff_lat);
-        var b_dropoff_lngDiff = Math.abs(request.dropoff_lng - a.dropoff_lng);
+        var b_timeDiff = Math.abs(request.time_calc - b.time_calc);
+        var b_pickup_latDiff = Math.abs(request.pickup_lat - b.pickup_lat);
+        var b_pickup_lngDiff = Math.abs(request.pickup_lng - b.pickup_lng);
+        var b_dropoff_latDiff = Math.abs(request.dropoff_lat - b.dropoff_lat);
+        var b_dropoff_lngDiff = Math.abs(request.dropoff_lng - b.dropoff_lng);
         var bDiff = b_timeDiff/hour + b_pickup_latDiff/lat2 + b_pickup_lngDiff/lng2 + b_dropoff_latDiff/lat2 + b_dropoff_lngDiff/lng2;
 
         return aDiff - bDiff;
@@ -38,18 +38,16 @@ var getResults = function(req, res) {
     } else {
         Request.findById(req.user.hasReq, function(err, request) {
             Request.find({
-                _id: { $ne: request._id },
-                date: request.date,
+                _id: { $ne: request._id }, // not itself
+                date: request.date, // same date
                 time_calc: { $gte: request.time_calc-hour, $lte: request.time_calc+hour }, // difference of 1 hour
                 pickup_lat: { $gte: request.pickup_lat-lat2, $lte: request.pickup_lat+lat2 }, // difference of ~2 miles
                 pickup_lng: { $gte: request.pickup_lng-lng2, $lte: request.pickup_lng+lng2 },
                 dropoff_lat: { $gte: request.dropoff_lat-lat2, $lte: request.dropoff_lat+lat2 },
                 dropoff_lng: { $gte: request.dropoff_lng-lng2, $lte: request.dropoff_lng+lng2 },
             }, function(err, results) {
-                // if (results) {
-                // };
                 var sorted = results.sort(compare(request));
-                res.render('results', { title: 'hitch | Results', results: sorted, message: req.flash('message') });
+                res.render('results', { title: 'hitch | Results', matches: sorted, message: req.flash('message') });
             });
         });
     };
