008ffdaf7a75966808512ff3adcaeadc6deeccd4
Larry Zhang <lzhang124@gmail.com>
2015-01-25 22:44:17 -0500

    express-session modification

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 var passport = require('passport');
 var expressSession = require('express-session');
 
-app.use(expressSession({secret: 'thisisasecret'}));
+app.use(expressSession({
+    secret: 'thisisasecret',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
