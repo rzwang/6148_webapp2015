f9122e16d66ad62a030ab3634c3ca2dd7a0aea6d
Larry Zhang <lzhang124@gmail.com>
2015-01-26 08:58:18 -0500

    reordering

--- app.js
+++ app.js
@@ -6,10 +6,26 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
 
+// mongoose
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+var mongodbUri = 'mongodb://heroku_app33380250:j71copckd4tl1vqhvj20nroolp@ds031551.mongolab.com:31551/heroku_app33380250';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri);
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // passport config
 var passport = require('passport');
@@ -27,24 +43,6 @@ app.use(flash());
 // passport init
 var initPassport = require('./passport/init');
 initPassport(passport);
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// mongoose
-var mongoose = require('mongoose');
-var uriUtil = require('mongodb-uri');
-var mongodbUri = 'mongodb://heroku_app33380250:j71copckd4tl1vqhvj20nroolp@ds031551.mongolab.com:31551/heroku_app33380250' || 'mongodb://localhost/passport';
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
-
-mongoose.connect(mongooseUri);
 
 // cron Database
 var clearDatabase = require('./controller/clearController');
