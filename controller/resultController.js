5e11908f1d28b6ac84213bd92274ee4b11c408a6
Larry Zhang <lzhang124@gmail.com>
2015-01-27 05:13:27 -0500

    more debug

--- controller/resultController.js
+++ controller/resultController.js
@@ -31,7 +31,7 @@ var getResults = function(req, res) {
     } else {
         console.log('before search');
         Request.findById(req.user.hasReq, function(err, request) {
-            console.log(currentTime);
+            console.log('hello', currentTime);
             Request.find({
                 _id: { $ne: request._id }, // not itself
                 date: request.date, // same date
