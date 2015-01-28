1e6716d595a22c16fa635152d37d4dc8cd4f9f00
Larry Zhang <lzhang124@gmail.com>
2015-01-27 23:20:04 -0500

    scale

--- controller/resultController.js
+++ controller/resultController.js
@@ -12,14 +12,14 @@ function compare(request) {
         var a_pickup_lngDiff = Math.abs(request.pickup_lng - a.pickup_lng);
         var a_dropoff_latDiff = Math.abs(request.dropoff_lat - a.dropoff_lat);
         var a_dropoff_lngDiff = Math.abs(request.dropoff_lng - a.dropoff_lng);
-        var aDiff = a_timeDiff/(3*hour) + a_pickup_latDiff/lat2 + a_pickup_lngDiff/lng2 + a_dropoff_latDiff/lat2 + a_dropoff_lngDiff/lng2;
+        var aDiff = a_timeDiff/hour + a_pickup_latDiff/lat2 + a_pickup_lngDiff/lng2 + a_dropoff_latDiff/lat2 + a_dropoff_lngDiff/lng2;
 
         var b_timeDiff = Math.abs(request.time_calc - b.time_calc);
         var b_pickup_latDiff = Math.abs(request.pickup_lat - b.pickup_lat);
         var b_pickup_lngDiff = Math.abs(request.pickup_lng - b.pickup_lng);
         var b_dropoff_latDiff = Math.abs(request.dropoff_lat - b.dropoff_lat);
         var b_dropoff_lngDiff = Math.abs(request.dropoff_lng - b.dropoff_lng);
-        var bDiff = b_timeDiff/(3*hour) + b_pickup_latDiff/lat2 + b_pickup_lngDiff/lng2 + b_dropoff_latDiff/lat2 + b_dropoff_lngDiff/lng2;
+        var bDiff = b_timeDiff/hour + b_pickup_latDiff/lat2 + b_pickup_lngDiff/lng2 + b_dropoff_latDiff/lat2 + b_dropoff_lngDiff/lng2;
 
         return aDiff - bDiff;
     };
